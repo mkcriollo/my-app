e5abecd01fbc2c3c65d331d8013c844fb0774e20
Mike Criollo <mkcriollo@gmail.com>
2023-11-12 19:24:38 -0500

    create a card like component for mobile view

--- src/Components/SinglePhoto.tsx
+++ src/Components/SinglePhoto.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GridItem, Image, Flex, Text } from "@chakra-ui/react";
+import { GridItem, Image, Flex, Text, Box, Button } from "@chakra-ui/react";
 import { IPhoto, ISingleImageProps } from "../Types";
 import { MdAddToPhotos } from "react-icons/md";
 
@@ -13,47 +13,70 @@ const SinglePhoto = (props: ISingleImageProps): JSX.Element => {
     drag,
     addToAlbum,
   } = props;
-  const { url, id, title } = photo;
+  const { url, id, title, description } = photo;
+
+  const handleButtonAdd = (): void => {
+    console.log(select?.selectImgs);
+    if (!select?.selectImgs.includes(photo)) {
+      addToAlbum(photo);
+    }
+  };
   return (
     <GridItem colSpan={{ base: canDrag ? Number(amountOfCols) : 1, lg: 1 }}>
-      <Image
-        cursor={select?.selectMode ? "pointer" : "default"}
-        id="single-image"
-        draggable={canDrag}
-        onDragStart={() => drag(photo)}
-        src={url}
-        key={id}
-        alt={title}
-        objectFit="cover"
-        width="100%"
-        opacity={
-          select?.selectImgs.some((curr: IPhoto) => {
-            return curr.id === photo.id;
-          })
-            ? 0.5
-            : 1
-        }
-        onClick={() => handleSelectMode(photo)}
-      />
-      {canDrag && (
-        <Flex
-          display={{ base: "flex", lg: "none" }}
-          h="100px"
-          background="lightgray"
-          justifyContent="space-between"
-          alignItems="center"
-          p="0px 15px"
-        >
-          <Text fontSize="14px" fontWeight="bold">
-            {title}
-          </Text>
-          <MdAddToPhotos
-            fontSize="18px"
-            cursor="pointer"
-            onClick={() => addToAlbum(photo)}
-          />
-        </Flex>
-      )}
+      <Box
+        p={{ base: canDrag ? "20px" : "0px", lg: "0px" }}
+        background="#F0F0F0"
+        borderRadius="6px"
+      >
+        <Image
+          borderRadius={{ base: canDrag ? "6px" : "0px", lg: "0px" }}
+          cursor={select?.selectMode ? "pointer" : "default"}
+          id="single-image"
+          draggable={canDrag}
+          onDragStart={() => drag(photo)}
+          src={url}
+          key={id}
+          alt={title}
+          objectFit="cover"
+          width="100%"
+          opacity={
+            select?.selectImgs.some((curr: IPhoto) => {
+              return curr.id === photo.id;
+            })
+              ? 0.5
+              : 1
+          }
+          onClick={() => handleSelectMode(photo)}
+        />
+        {canDrag && (
+          <Flex
+            display={{ base: "flex", lg: "none" }}
+            h="200px"
+            background="#F0F0F0"
+            justifyContent="space-between"
+            flexDirection="column"
+            p="15px 0px"
+          >
+            <Text fontSize="2xl" fontWeight="bold">
+              {title}
+            </Text>
+            <Text>{description}</Text>
+            <Button
+              borderRadius="6px"
+              p="15px"
+              background="#a3e7e7"
+              onClick={() => handleButtonAdd()}
+              _hover={{
+                background: "#339f9f",
+                border: "none",
+                color: "white",
+              }}
+            >
+              Add to Album
+            </Button>
+          </Flex>
+        )}
+      </Box>
     </GridItem>
   );
 };
